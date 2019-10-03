1e0edd51983390bafcb0b63a1bb8dc016c916dd2
Enrica <enrica.s92@gmail.com>
2019-10-03 18:46:59 +0200

    Added test of compute neighborhood with casadi objects

--- examples/acados_matlab_octave/swarming/test_compute_neig_casadi.m
+++ examples/acados_matlab_octave/swarming/test_compute_neig_casadi.m
@@ -1,3 +1,5 @@
+import casadi.*
+
 N = 3;
 max_neig = 2;
 r_comm = 150;
@@ -5,8 +7,10 @@ rng(4)
 position = rand(3*N,1);
 
 
-dist2_matrix = zeros(N,N);
-M = zeros(N,N);
+N = length(position)/3;
+dist2_matrix = SX.sym('D',N,N);
+
+M = SX.zeros(N,N);
 
 for agent = 1:N
     for neig = 1:N
@@ -21,37 +25,34 @@ for agent = 1:N
     end
 end
 
-sorted_neig = zeros(max_neig,N);
-
+sorted_neig = SX.zeros(max_neig,N);
 for i = 1:N % for every column, order column
     
-    to_sort = ones(N,1);
+    to_sort = SX.ones(N,1);
     dist2_to_sort = dist2_matrix(:,i);
     for j = 1:max_neig % order only the first max_neig
         min_idx = 0;
-        min_dist2 = 1e9; % high init value
+        min_dist2 = SX(1e9); % high init value
         for k = 1:N
-            if dist2_to_sort(k) < min_dist2 && ...
-                    dist2_to_sort(k) < r_comm^2 && ...
-                    to_sort(k)
-                min_idx = k;
-            end
-            if dist2_to_sort(k) < min_dist2 && ...
-                    dist2_to_sort(k) < r_comm^2 && ...
-                    to_sort(k)
-                min_dist2 = dist2_to_sort(k);
-            end
+            min_idx = if_else(dist2_to_sort(k) < min_dist2 & ...
+                dist2_to_sort(k) < r_comm^2 & ...
+                to_sort(k), ...
+                k, min_idx);
+            min_dist2 = if_else(dist2_to_sort(k) < min_dist2 & ...
+                dist2_to_sort(k) < r_comm^2 & ...
+                to_sort(k), ...
+                dist2_to_sort(k), min_dist2);
         end
         sorted_neig(j,i) = min_idx;
         for l = 1:N
-            if min_idx == l
-                to_sort(l) = 0;
-                M(l,i) = 1;
-            end
+            to_sort(l) = if_else(min_idx == l , ...
+                0, to_sort(l));
+            M(l,i) = if_else(min_idx == l, ...
+                1, M(l,i));
         end
-        
     end
 end
+
 
 disp(dist2_matrix)
 disp(sorted_neig)
